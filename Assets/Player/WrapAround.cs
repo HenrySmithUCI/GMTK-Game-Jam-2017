ba16cccf8d4dd22e20f9985334fa1cbd22229afc
danhn255 <danhn255@gmail.com>
2017-07-15 16:15:52 -0700

    Delete Wrap Around Meta Data
    Fixed Wrap Around Script

--- Assets/Player/WrapAround.cs
+++ Assets/Player/WrapAround.cs
@@ -12,17 +12,11 @@ public class WrapAround : MonoBehaviour {
     float halfWidth;
     float halfHeight;
     Vector2[] directions = new Vector2[4];
-<<<<<<< HEAD:Assets/WrapAround.cs
-
-=======
-    //Rigidbody2D mainRB;
->>>>>>> 7f09571ee29e5c5bb0ef50ad716dd1fd8124c43e:Assets/Player/WrapAround.cs
 
 	void Start () {
         Matrix4x4 orthoMatrix = Camera.main.projectionMatrix;
         halfWidth = (2/orthoMatrix[0]) / 2;
         halfHeight = (2 / orthoMatrix[5]) / 2;
-<<<<<<< HEAD:Assets/WrapAround.cs
         directions[0] = new Vector2(halfWidth, halfHeight);
         directions[0].Normalize();
         directions[1] = new Vector2(-halfWidth, halfHeight);
@@ -31,14 +25,10 @@ public class WrapAround : MonoBehaviour {
         directions[2].Normalize();
         directions[3] = new Vector2(halfWidth, -halfHeight);
         directions[3].Normalize();
-
-=======
         directions[0] = new Vector2(1, 1);
         directions[1] = new Vector2(-1, 1);
         directions[2] = new Vector2(-1, -1);
         directions[3] = new Vector2(1, -1);
-        //mainRB = GetComponent<Rigidbody2D>();
->>>>>>> 7f09571ee29e5c5bb0ef50ad716dd1fd8124c43e:Assets/Player/WrapAround.cs
     }
 	
 
@@ -72,16 +62,10 @@ public class WrapAround : MonoBehaviour {
         if (shouldTransform)
         {
             float pointer = Vector2.Dot(posNorm, directions[0]);
-<<<<<<< HEAD:Assets/WrapAround.cs
             float pointer2 = Vector2.Dot(posNorm, directions[3]);
             float pointer3 = Vector2.Dot(posNorm, directions[1]);
             float pointer4 = Vector2.Dot(posNorm, directions[2]);
-=======
-            //float pointer2 = Vector2.Dot(posNorm, directions[3]);
-            //float pointer3 = Vector2.Dot(posNorm, directions[1]);
-            //float pointer4 = Vector2.Dot(posNorm, directions[2]);
 
->>>>>>> 7f09571ee29e5c5bb0ef50ad716dd1fd8124c43e:Assets/Player/WrapAround.cs
             if (posTransform.x < pos.x)
             {
                 if (pointer >= cornerThreshAngle && pos.y > (halfHeight - cornerThresholdY))
