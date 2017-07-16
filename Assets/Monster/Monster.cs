3e90fa64c13aebfbe643cb4f3e496c67f27dcadb
HenrySmithUci <hgsmith@uci.edu>
2017-07-15 18:42:01 -0700

    Enemies Changes

--- Assets/Monster/Monster.cs
+++ Assets/Monster/Monster.cs
@@ -3,5 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Monster : MonoBehaviour {
+    public GameObjectSpawnPool pool;
+
     public abstract void init(Vector2 pos, Vector2 vals, string t = "Player");
+
+    public void die()
+    {
+        gameObject.SetActive(false);
+        Bullet b = pool.getInactivePooledObject().GetComponent<Bullet>();
+        b.init(transform.position, Random.Range(0, 360));
+    }
 }
